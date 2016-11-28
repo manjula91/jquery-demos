0666f1377e5097546b48256a585a5ec0cfb8b9c3
amite <amit.erandole@gmail.com>
2016-11-28 08:06:06 +0530

    added browsersync and basic gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+
+// Static server
+gulp.task('serve', function () {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./src"
+        }
+    });
+
+    // add browserSync.reload to the tasks array to make
+    // all browsers reload after tasks are complete.
+    gulp.watch(["./src/*.html", "./src/css/*.css", "./src/js/*.js"]).on("change", reload);
+});
