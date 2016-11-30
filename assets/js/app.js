ea2d2d8019a1f5c052e15cda6653e2f9993d61a4
amite <amit.erandole@gmail.com>
2016-11-30 20:57:07 +0530

    setup for demo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,25 @@
 var app = new Vue({
   el: '#app',
+
   data: {
-    purchased: false,
+    radius: 20,
+    users: [],
   },
+
+  created : function() {
+    this.fetchUsers();
+  },
+  
   methods: {
-    purchase: function() {
-      this.purchased = true;
+    fetchUsers: function() {
+      var self = this;
+      fetch('/assets/data/radfaces.json')
+        .then(function(response) { return response.json()
+        .then( function(data) { self.users = data; } );
+      })
+        .catch(function(error) {
+        console.log(error);
+      });
     }
   }
 });
