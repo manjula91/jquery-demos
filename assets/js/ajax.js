9baee1419715d4f577d345f5b89db8b60e0eeaf1
Manjula Subhashchandra Dube <dube.manjula668@gmail.com>
2016-12-03 19:06:37 +0530

    Code Refactoring

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,18 +1,32 @@
-$( document ).ready(function() {
-    $( ".btn-click" ).on( "click", function() {
-  		$.ajax({
-		  url: 'https://randomuser.me/api/?results=10',
-		  dataType: 'json',
-		  success: function(data) {
-		    $.each(data.results,function(key,value){
-		    	var userImage = value.picture['medium'];
-		    	var sFirstName =  value.name['first'];
-		    	var sLastName =  value.name['last'];
-		    	var gender = value.gender;
-		    	$(".user-list-container").append(`<li data-src="${gender}"><div><img src="${userImage}"/></div>${sFirstName} ${sLastName} </li>`)
-		    })
-		  }
-		});
-	});
+var userProfile = userProfile || {};
 
-});
+
+userProfile.init = function(){
+	$( ".btn-click" ).on( "click", function() {
+		userProfile.getListOfUsers();
+	});
+}
+
+userProfile.getListOfUsers = function(){
+	$.ajax({
+	  url: 'https://randomuser.me/api/?results=10',
+	  dataType: 'json',
+	  success: function(data) {
+	    userProfile.ProcessResultsForEachUser(data.results)
+	  }
+	});
+}
+
+userProfile.ProcessResultsForEachUser = function(UserList){
+	$.each(UserList,function(key,value){
+		var userImage = value.picture['medium'];
+		var sFirstName =  value.name['first'];
+		var sLastName =  value.name['last'];
+		var gender = value.gender;
+		$(".user-list-container").append(`<li data-src="${gender}"><div><img src="${userImage}"/></div>${sFirstName} ${sLastName} </li>`)
+	})
+}
+
+$(document).ready(function(){
+	userProfile.init();
+})
