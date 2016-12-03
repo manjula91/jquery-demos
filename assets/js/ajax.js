61cd85c017e5a20796cd700a3643c1c6700a21b1
Manjula Subhashchandra Dube <dube.manjula668@gmail.com>
2016-12-03 18:48:05 +0530

    DIRTY CODE

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,7 +1,18 @@
 $( document ).ready(function() {
     $( ".btn-click" ).on( "click", function() {
-  		
-    
+  		$.ajax({
+		  url: 'https://randomuser.me/api/?results=10',
+		  dataType: 'json',
+		  success: function(data) {
+		    $.each(data.results,function(key,value){
+		    	var userImage = value.picture['medium'];
+		    	var sFirstName =  value.name['first'];
+		    	var sLastName =  value.name['last'];
+		    	var gender = value.gender;
+		    	$(".user-list-container").append(`<li data-src="${gender}"><div><img src="${userImage}"/></div>${sFirstName} ${sLastName} </li>`)
+		    })
+		  }
+		});
 	});
 
 });
